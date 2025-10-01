7c4a4921dce5baee9e00045b37feab7be526daf0
tonigith <91883442+toniGitH@users.noreply.github.com>
2025-10-01 08:27:47 +0200

    Creada estructura base para contexto admin y entidad user

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::prefix('admin/user')->group(base_path('src/admin/user/infrastructure/routes/api.php'));
---
+++ src/admin/user/infrastructure/controllers/ExampleGETController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Src\admin\user\infrastructure\controllers;
+
+use App\Http\Controllers\Controller;
+
+final class ExampleGETController extends Controller { 
+
+ public function index() { 
+ // TODO: DDD Controller content here 
+ }
+}
